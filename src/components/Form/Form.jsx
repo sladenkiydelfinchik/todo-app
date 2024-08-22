1043dab451ace02bdabb4ea0b539ccbf863c0422
Kenstontin <sladenkiydelfinchik@gmail.com>
2024-08-22 17:57:40 +0300

    Update plusIcon

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,16 @@
 import styles from './Form.module.css'
+import plusIcon from '../../shared/icons/plus.svg'
 
 const Form = () => {
     return (
         <div className={styles.FormWrapper}>
-       <form className={styles.Form} action=""
-            onSubmit={e => e.preventDefault()}>
-        <span className={styles.FormHeader}>Новая задача</span>
-        <input className={styles.FormInput} type="text" placeholder="Введите текст"/>
-        <button className={styles.FormSubmit}> Создать</button>
-       </form>
-       </div>
+            <form className={styles.Form} action=""
+                onSubmit={e => e.preventDefault()}>
+                <span className={styles.FormHeader}>Новая задача</span>
+                <input className={styles.FormInput} type="text" placeholder="Введите текст" />
+                <button className={styles.FormSubmit}> <img src={plusIcon} /> Создать</button>
+            </form>
+        </div>
     );
 }
 
